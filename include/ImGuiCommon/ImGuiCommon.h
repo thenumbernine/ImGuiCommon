7335751b3ac08dd0764da8ad8bb4a51e79b8aaf2
Christopher Moore <christopher.e.moore@gmail.com>
2019-05-29 13:49:09 +0800

    updated to work with latest cimgui/imgui

--- include/ImGuiCommon/ImGuiCommon.h
+++ include/ImGuiCommon/ImGuiCommon.h
@@ -2,35 +2,47 @@
 
 //This is just a class wrapper of imgui_impl_sdl.cpp/.h
 
-#include "imgui.h"	//IMGUI_API
+#include "cimgui.h"
 #include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl2.h"
 #include "Common/gl.h"
 #include <functional>
+#include <SDL.h>
 
 namespace ImGuiCommon {
 
 struct ImGuiCommon {
 	SDL_Window* window;
 
-	ImGuiCommon(SDL_Window* window_)
+	ImGuiCommon(SDL_Window* window_, SDL_GLContext context)
 	: window(window_)
 	{
-		ImGui_ImplSdl_Init(window);
+		igCreateContext(nullptr);
+		igStyleColorsDark(nullptr);
+		ImGui_ImplSDL2_InitForOpenGL(window, context);
+		ImGui_ImplOpenGL2_Init();
 	}
 
 	virtual ~ImGuiCommon() {
-		ImGui_ImplSdl_Shutdown();
+		ImGui_ImplOpenGL2_Shutdown();
+		ImGui_ImplSDL2_Shutdown();
+		igDestroyContext(nullptr);
 	}
 
 	void sdlEvent(SDL_Event& event) {
-		ImGui_ImplSdl_ProcessEvent(&event);
+		ImGui_ImplSDL2_ProcessEvent(&event);
 	}
 
 	void update(std::function<void()> callback) {
-		ImGui_ImplSdl_NewFrame(window);
+		ImGui_ImplOpenGL2_NewFrame();
+		ImGui_ImplSDL2_NewFrame(window);
+		igNewFrame();
+	
 		callback();	
-		glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
-		ImGui::Render();
+		glViewport(0, 0, (int)igGetIO()->DisplaySize.x, (int)igGetIO()->DisplaySize.y);
+		
+		igRender();
+		ImGui_ImplOpenGL2_RenderDrawData(igGetDrawData());
 	}
 };
 
